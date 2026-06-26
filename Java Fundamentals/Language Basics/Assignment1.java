2135adce09a7ab5386f54ff98627ec1f1fd19b37
Yashthakare65 <yash44thakare@gmail.com>
2026-06-26 23:16:06 +0530

    Questions also commented in a program for better understanding

--- Java Fundamentals/Language Basics/Assignment1.java
+++ Java Fundamentals/Language Basics/Assignment1.java
@@ -1,3 +1,8 @@
+//  Write a Program that accepts two Strings as command line arguments and generate the output in the required format. 
+
+// Example1
+// If the two command line arguments are Wipro and Bangalore then the output generated should be Wipro Technologies Bangalore.
+
 class Assignment1{
   public static void main(String args[]){
     if(args.length != 2 ){
